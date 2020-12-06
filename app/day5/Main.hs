4e9431d52a91d9097a6abf3bde3b96f77a45e483
Pepijn Overbeeke <pepijn.overbeeke@gmail.com>
2020-12-06 12:29:50 +0100

    Day 5

---
+++ app/day5/Main.hs
@@ -0,0 +1,26 @@
+module Main where
+
+import Lib
+import Data.List
+
+parseBinary :: [Char] -> Int -> String -> Int
+parseBinary chars n [] = n
+parseBinary chars n (x:xs)
+  | x `elem` chars = parseBinary chars (2 * n + 1) xs
+  | otherwise      = parseBinary chars (2 * n) xs
+
+parseID :: String -> Int
+parseID = parseBinary "BR" 0
+
+solve1 :: [String] -> Int
+solve1 = maximum . map parseID
+
+solve2 :: [String] -> Int
+solve2 xs = head $ (\\) [min .. max] ids
+  where
+    ids = map parseID xs
+    max = maximum ids
+    min = minimum ids
+
+main :: IO()
+main = mainWrapper "day5" [solve1, solve2]
