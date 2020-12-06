ea25abdd29b0e3760a8d675b13da0bf2fe542501
Pepijn Overbeeke <pepijn.overbeeke@gmail.com>
2020-12-06 22:53:41 +0100

    Use better parsing for day 2

--- app/Day2.hs
+++ app/Day2.hs
@@ -1,41 +1,45 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Lib
 import Data.List.Split
+import Data.Attoparsec.Text (Parser, char, anyChar, decimal, parseOnly, space, many1, letter)
+import Prelude hiding (lines)
+import Data.Text (Text, pack, lines)
 
 data Line = Line {
     mini :: Int,
     maxi :: Int,
-    char :: Char,
+    ch :: Char,
     password :: String
 } deriving (Show)
 
-parseLine :: String -> Line
-parseLine l = Line { mini=minis, maxi=maxis, char=ch, password=passw }
-  where w = words l
-        ch = head . head . tail $ w
-        mm = splitOn "-" $ head w
-        minis = (read $ head mm) :: Int
-        maxis = (read $ last mm) :: Int
-        passw = last w
+parseLine :: Parser Line
+parseLine = Line <$> decimal <*> ("-" *> decimal <* space) <*> (anyChar <* ": ") <*> (many1 letter)
+
+unsafeParse :: Text -> Line
+unsafeParse = fromRight . parseOnly parseLine
+  where fromRight (Right a) = a
+        fromRight b = error $ "failed to parse " ++ (show b)
 
 checkPassword1 :: Line -> Bool
 checkPassword1 l = inRange c (mini l) (maxi l)
   where
-    c = length . filter (== (char l)) $ password l
+    c = length . filter (== (ch l)) $ password l
 
 solve1 :: [String] -> Int
-solve1 = sum . map (fromEnum . checkPassword1 . parseLine)
+solve1 = sum . map (fromEnum . checkPassword1 . unsafeParse . pack)
 
 checkPassword2 :: Line -> Bool
 checkPassword2 l = (f == c) /= (a == c)
   where
     f = (password l) !! ((mini l) - 1)
     a = (password l) !! ((maxi l) - 1)
-    c = char l
+    c = ch l
 
 solve2 :: [String] -> Int
-solve2 = sum . map (fromEnum . checkPassword2 . parseLine)
+solve2 = sum . map (fromEnum . checkPassword2 . unsafeParse . pack)
 
 main :: IO ()
 main = mainWrapper "day2" [solve1, solve2]
