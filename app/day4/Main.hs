1af2b999ee1c9df3ab07b4043a740fad3692b79f
Pepijn Overbeeke <pepijn.overbeeke@gmail.com>
2020-12-06 12:29:32 +0100

    Day 4

---
+++ app/day4/Main.hs
@@ -0,0 +1,54 @@
+module Main where
+
+import Lib
+import Data.Char
+import Data.List.Split
+
+data KeyValue = KeyValue {
+  key :: String,
+  value :: String
+} deriving (Show, Eq)
+
+type Passport = [KeyValue]
+
+isValid :: KeyValue -> Bool
+isValid kp
+  | k == "byr" = inRange (read v) 1920 2002
+  | k == "iyr" = inRange (read v) 2010 2020
+  | k == "eyr" = inRange (read v) 2020 2030
+  | k == "hgt" = (xx == "cm" && inRange yy 150 193) || (xx == "in" && inRange yy 59 76)
+  | k == "hcl" = (head v == '#') && (all (isHexDigit . toLower) $ tail v) && (length v == 7)
+  | k == "ecl" = v `elem` ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+  | k == "pid" = (length v == 9) && (all isDigit v)
+  | otherwise = True
+  where k = key kp
+        v = value kp
+        xx = drop (length v - 2) v
+        yy = read $ take (length v - 2) v
+
+toKeyValue :: [String] -> KeyValue
+toKeyValue (x:xs) = KeyValue { key = x, value = head xs }
+
+grouping :: [String] -> [[String]]
+grouping = map (concat . map words) . groupPairs
+
+parse :: [String] -> Passport
+parse = map (toKeyValue . splitOn ":")
+
+validPassport1 :: Passport -> Bool
+validPassport1 pp = (l == 8) || ((l == 7) && not ("cid" `elem` keys))
+  where
+    keys = map key pp
+    l = length keys
+
+solve1 :: [String] -> Int
+solve1 = length . filter validPassport1 . map parse . grouping
+
+validPassport2 :: Passport -> Bool
+validPassport2 pp = validPassport1 pp && (all isValid pp)
+
+solve2 :: [String] -> Int
+solve2 = length . filter validPassport2 . map parse . grouping
+
+main :: IO()
+main = mainWrapper "day4" [solve1, solve2]
