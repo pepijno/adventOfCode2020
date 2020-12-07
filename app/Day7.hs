80f98e5f01f3ae8d05c84ceb79e4e16e493f065a
Pepijn Overbeeke <pepijn.overbeeke@gmail.com>
2020-12-07 21:16:27 +0100

    Day 7

---
+++ app/Day7.hs
@@ -0,0 +1,58 @@
+module Main where
+
+import Lib
+import Control.Applicative
+import Data.Maybe
+import qualified Data.Map as M
+import qualified Data.Set as S
+
+parseName :: Parser String
+parseName = (++) <$> stringLiteral <*> (whiteSpace *> stringLiteral)
+
+parseChild :: Parser (Int, String)
+parseChild = (,) <$>
+  (integer <* whiteSpace) <*>
+  (parseName <* bags <* delimiter)
+    where bags = string " bags" <|> string " bag"
+          delimiter = char '.' <|> char ','
+
+parseChildren :: Parser [(Int, String)]
+parseChildren = noChildren <|> ((:) <$> parseChild <*> many (whiteSpace *> parseChild) <|> pure [])
+  where noChildren = pure [] <* string "no other bags."
+
+parseInput :: Parser (String, [(Int, String)])
+parseInput = (,) <$> (parseName <* sep) <*> parseChildren
+  where sep = string " bags contain "
+
+parseAll :: [String] -> [(String, [(Int, String)])]
+parseAll = map fst . fromJust . sequenceA . map (parse parseInput)
+
+createParentMap :: M.Map String [String] -> (String, [String]) -> M.Map String [String]
+createParentMap m item = foldl (\n x -> M.insert x ((:) parent $ getItem n x) n) m children
+  where parent = fst item
+        children = snd item
+        getItem n x = M.findWithDefault [] x n
+
+countUniqueParents :: M.Map String [String] -> S.Set String -> String -> S.Set String
+countUniqueParents m s key =
+  foldl f (S.insert key s) (M.findWithDefault [] key m)
+    where f visited next
+            | S.member next visited = visited
+            | otherwise = countUniqueParents m visited next
+
+solve1 :: [String] -> Int
+solve1 input = flip (-) 1 . S.size $ countUniqueParents (foldl createParentMap M.empty conv') S.empty "shinygold"
+  where conv' = map (\(x, y) -> (x, map snd y)) $ parseAll input
+
+countChildren :: String -> M.Map String [(Int, String)] -> Int
+countChildren key m
+  | length children == 0 = 0
+  | otherwise = sum $ map countChild children
+  where children = M.findWithDefault [] key m
+        countChild (x, y) = x * (1 + countChildren y m)
+
+solve2 :: [String] -> Int
+solve2 = countChildren "shinygold" . M.fromList . parseAll
+
+main :: IO()
+main = mainWrapper "day7" [solve1, solve2]
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,10 +1,20 @@
 module Lib (
   mainWrapper,
   inRange,
-  groupPairs
+  groupPairs,
+
+  Parser,
+  parse,
+  char,
+  string,
+  integer,
+  stringLiteral,
+  whiteSpace
 ) where
 
+import Data.Char
 import Data.List.Split
+import Control.Applicative
 
 mainWrapper :: (Show a) => String -> [[String] -> a] -> IO()
 mainWrapper file fs = do
@@ -16,3 +26,43 @@ inRange v min max = min <= v && v <= max
 
 groupPairs :: [String] -> [[String]]
 groupPairs = splitOn [""]
+
+newtype Parser a = Parser { parse :: String -> Maybe (a, String) }
+
+instance Functor Parser where
+  fmap f (Parser p) = Parser $ \input -> do
+    (x, rest) <- p input
+    Just (f x, rest)
+
+instance Applicative Parser where
+  pure x = Parser $ \input -> Just (x, input)
+  (Parser a) <*> (Parser b) = Parser $ \input -> do
+    (f, rest) <- a input
+    (x, rest') <- b rest
+    Just (f x, rest')
+
+instance Alternative Parser where
+  empty = Parser $ const Nothing
+  (Parser a) <|> (Parser b) = Parser $ \input -> (a input) <|> (b input)
+
+char :: Char -> Parser Char
+char c = Parser f
+  where f (x:rest)
+          | x == c = Just (x, rest)
+          | otherwise = Nothing
+        f [] = Nothing
+
+string :: String -> Parser String
+string = sequenceA . map char
+
+splitBy :: (Char -> Bool) -> Parser String
+splitBy f = Parser $ \input -> Just $ span f input
+
+integer :: Parser Int
+integer = read <$> splitBy isDigit
+
+whiteSpace :: Parser String
+whiteSpace = splitBy isSpace
+
+stringLiteral :: Parser String
+stringLiteral = splitBy isLetter
